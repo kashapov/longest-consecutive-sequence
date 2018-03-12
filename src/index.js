820632ef2bb8722b62afc6a47a60aba3fb710d40
Andrey Kashapov <kashapovandrey@gmail.com>
2018-03-12 20:21:19 +0300

    longest ... v.1.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  
+  var sortArray = array.sort(function(a, b) {
+    return a - b;
+  });
+
+  //console.log(sortArray);
+  
+  var sequenceLength = 1;
+  var sequenceLengthSwap = 1;
+
+  if (!sortArray.length) {
+    return 0;
+  } else {
+
+    for (var i = 0; i < sortArray.length; i++) {
+      if (sequenceLengthSwap > sequenceLength) {
+        sequenceLength = sequenceLengthSwap;
+        sequenceLengthSwap = 1;    
+      } else if (sortArray[i] == sortArray[i+1] - 1) {
+        sequenceLengthSwap++;       
+      } else {
+        sequenceLengthSwap = 1;
+      }
+    }
+
+  }
+
+  
+
+  //console.log(length);
+
+  
+  return sequenceLength;
+
 }
